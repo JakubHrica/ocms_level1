1cb6dc7126bc1a89506917cc75f223505cefe180
JakubHrica <kubo.hrica@gmail.com>
2025-05-06 21:07:59 +0200

    version-3

--- plugins/applogger/logger/routes.php
+++ plugins/applogger/logger/routes.php
@@ -4,34 +4,33 @@ namespace AppLogger\Logger\Classes\Routes;
 use AppLogger\Logger\Models\Log;
 use Illuminate\Support\Facades\Route;
 
-
 // Route pre získanie všetkých logov
 Route::get('/logs', function() {
-    // Získať všetky logy z databázy
     $logs = Log::all();
-
-    // Vrátiť logy ako JSON odpoveď
     return $logs;
 });
 
 // Route pre získanie logu podľa ID
 Route::get('/logs/{id}', function($id) {
-    // Získať log podľa ID z databázy
     $log = Log::findOrFail($id);
-
-    // Vrátiť log ako JSON odpoveď
     return $log;
 });
 
 // Route pre vytvorenie nového logu
 Route::post('/logs', function() {
     $log = new Log();
-
     $data = post();
-
     $log->fill($data);
+
+    $arrivalTimestamp = strtotime($data['arrival']);
+    $currentTime = date('H:i:s');
+    $isLate = $arrivalTimestamp > strtotime($currentTime . ' 08:00:00');
+    $log->late = $isLate;
 
     $log->save();
 
-    return $log;
+    return response()->json([
+        'log' => $log,
+        'is_late' => $log->late
+    ]);
 });
